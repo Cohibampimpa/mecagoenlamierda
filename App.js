b69316213c2d5b5c64d875be8ca5fc5134efe26e
Cohibampimpa <genovevagordillo@gmail.com>
2025-07-02 21:50:22 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-300 via-purple-400 to-orange-300 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white text-center p-8">
+        ME CAGO EN LA MIERDA
+        <br/>
+        <span className="text-lg font-normal block mt-4">Un proyecto de Agnese</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
